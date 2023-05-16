2a8a19f59d344ccb86c7e423088925f4b871876b
dzc0d3r <m4dcod3r@protonmail.com>
2023-05-16 17:02:32 +0100

    finished UI Components

---
+++ src/components/Demo.jsx
@@ -0,0 +1,54 @@
+
+import { useState, useEffect } from 'react'
+import { copy, linkIcon, loader, tick } from '../assets'
+
+const Demo = () => {
+
+    const [article, setArticle] = useState({
+        url: '',
+        summary: ''
+    })
+
+    const handleSubmit = async (e) => {
+
+
+        alert(`submitted ${article.url}`)
+
+    }
+    return (
+        <section className="mt-16 w-full max-w-xl">
+
+
+            {/* search */}
+            <div className="flex flex-col w-full gap-2">
+                <form
+                    className="relative flex justify-center items-center"
+                    onSubmit={handleSubmit}>
+
+                    <img src={linkIcon} alt="link_icon" className="absolute left-0 my-2 ml-3 w-5" />
+                    <input
+                        type="url"
+                        placeholder='Please Enter a Valid URL'
+                        value={article.url}
+                        onChange={(e) => setArticle({ ...article, url: e.target.value })}
+                        required
+                        className="url_input peer"
+
+                    />
+                    <button
+                        type='submit'
+                        className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700'>
+                        🔥
+                    </button>
+                </form>
+
+                {/* browse url history */}
+
+            </div>
+            {/*  display results */}
+
+        </section>
+    )
+}
+
+export default Demo
